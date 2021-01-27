45bb076a4d1e4620afe8fb71ea1d6c1413267259
cdemarti <codythomasdemartin@gmail.com>
2021-01-27 00:41:25 -0500

    adds handle_in functions for param_ping event

--- hello_sockets/lib/hello_sockets_web/channels/ping_channel.ex
+++ hello_sockets/lib/hello_sockets_web/channels/ping_channel.ex
@@ -28,6 +28,14 @@ defmodule HelloSocketsWeb.PingChannel do
     {:stop, :shutdown, {:ok, %{msg: "shutting down"}}, socket}
   end
 
+  def handle_in("param_ping", %{"error" => true}, socket) do
+    {:reply, {:error, %{reason: "You asked for this!"}}, socket}
+  end
+
+  def handle_in("param_ping", payload, socket) do
+    {:reply, {:ok, payload}, socket}
+  end
+
   def handle_out("request_ping", payload, socket) do
     push(socket, "send_ping", Map.put(payload, "from_node", Node.self()))
     {:noreply, socket}
