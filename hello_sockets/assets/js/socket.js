d5ca52c6d9b7105479afb850f475e7218981bb8f
cdemarti <codythomasdemartin@gmail.com>
2021-01-27 00:36:17 -0500

    handles eroor ok and timeout reponse from pong pushe

--- hello_sockets/assets/js/socket.js
+++ hello_sockets/assets/js/socket.js
@@ -10,5 +10,14 @@ channel.join()
     .receive("ok", (resp) => {console.log("Joined ping channel"), resp })
     .receive("error", (resp) => {console.log("Unable to join ping channel", resp) })
 
+console.log("send ping")
+channel.push("ping", {})
+    .receive("ok", (resp) => console.log("receive", resp.ping));
+
+console.log("send pong")
+channel.push("pong", {})
+    .receive("ok", (resp) => console.log("won't happen"))
+    .receive("error", (resp) => console.error("won't happen yet"))
+    .receive("timeout", (resp) => console.error("pong message timeout", resp));
 export default socket
 
